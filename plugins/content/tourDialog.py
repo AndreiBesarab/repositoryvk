0b1bf221bf04d78c9a9d143dcc232eefdcbeeaff
AndreiBesarab <altandre@mail.ru>
2018-07-06 23:30:14 +0300

    1
    1

---
+++ plugins/content/tourDialog.py
@@ -0,0 +1,30 @@
+from handler.base_plugin_command import CommandPlugin
+import random
+
+class tourInDialog(CommandPlugin):
+    __slots__ = ()
+
+    def __init__(self, *commands, prefixes=None, strict=False):
+        super().__init__(*commands, prefixes=prefixes, strict=strict)
+
+        self.description = [
+            "Тур в диалоге", f"{self.command_example()} [запрос] - тур по городу"
+        ]
+
+    async def process_message(self, msg):
+       # data = await self.api.video.search(
+       #     q=self.parse_message(msg, full_text=True)[1] or "милый котик",
+       #     sort=2,
+       #     count=10,
+       #     adult=0,
+       #     offset= random.randint(1, 300)
+       # )
+
+        #if not data or not data.get("items"):
+        #    return await msg.answer("Я не могу получить видео или ничего не нашлось!")
+
+        text = 'хоба'
+
+        return await msg.answer(
+            text
+        )
